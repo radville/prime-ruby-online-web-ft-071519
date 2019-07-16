83360d9253f9a7e904a7ad2ed07605546e7d32e2
Laura Radville <laura.radville@gmail.com>
2019-07-16 16:32:16 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+require "pry"
+
+def prime?(num)
+  if num <= 1
+    return false
+  else
+    (2...num).each do |i|
+      if num % i == 0
+        return false
+      end
+    end
+  true
+  end
+end
